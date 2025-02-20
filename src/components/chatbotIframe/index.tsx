3f4b0ce1861d7b90060aa8de79c3786a464cfe9d
jigsaw <jigsawsmma@gmail.com>
2025-02-20 20:30:15 +0530

    chatbot working fully

--- src/components/chatbotIframe/index.tsx
+++ src/components/chatbotIframe/index.tsx
@@ -8,7 +8,7 @@ const ChatbotIframe = () => {
 
     const iframe = document.createElement("iframe");
 
-    const iframeStyles = (styleString:any) => {
+    const iframeStyles = (styleString: string) => {
       const style = document.createElement('style');
       style.textContent = styleString;
       document.head.append(style);
@@ -20,24 +20,32 @@ const ChatbotIframe = () => {
         bottom: 20px;
         right: 20px;
         border: none;
-        z-index: 999999;
+        z-index: 999;
+        max-width: 350px;
+        max-height: 646px;
+        border-radius: 10px;
       }
     `);
 
-    iframe.src = "https://jj-smartrep.vercel.app/chatbot";
+    iframe.src = "https://chatbot-jigsid.vercel.app/chatbot";
     iframe.classList.add('chat-frame');
     document.body.appendChild(iframe);
 
-    const handleMessage = (e:any) => {
-      if (e.origin !== "https://jj-smartrep.vercel.app") return null;
+    const handleMessage = (e: MessageEvent) => {
+      if (e.origin !== "https://chatbot-jigsid.vercel.app") return null;
+      
       try {
         const dimensions = JSON.parse(e.data);
-        iframe.style.width = dimensions.width + "px";
-        iframe.style.height = dimensions.height + "px";
+        iframe.style.width = dimensions.width + 'px';
+        iframe.style.height = dimensions.height + 'px';
       } catch (error) {
         console.error('Invalid message data:', e.data);
       }
-      iframe.contentWindow?.postMessage("2531aab1-1ea1-446e-8e7e-bedf41ad9021", "https://jj-smartrep.vercel.app/");
+      
+      iframe.contentWindow?.postMessage(
+        "408253b7-57fe-4f3d-a24b-6d401e246055", 
+        "https://chatbot-jigsid.vercel.app"
+      );
     };
 
     window.addEventListener("message", handleMessage);
