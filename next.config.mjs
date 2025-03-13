15ff5a31346c0057081cf22859b9da0a084a2b0e
jigsaw <jigsawsmma@gmail.com>
2025-03-14 03:53:38 +0530

    Add image domains and remote patterns for external image sources in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,17 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
+    domains: ['ucarecdn.com', 'wordpress-1273216-4599034.cloudwaysapps.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'ucarecdn.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'wordpress-1273216-4599034.cloudwaysapps.com',
+        pathname: '/**',
       },
     ],
   },
